2040072f5c05ab638e2deea003109d3d57d64050
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-11-15 11:07:46 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Recipes } from './recipes/recipes';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Profile } from './profile/profile';
+import { SavedRecipes } from './saved-recipes/saved-recipes';
+import { ViewRecipe } from './view-recipe/view-recipe';
+import { Pnf } from './pnf/pnf';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home,title:'Cookpedia - Home'},
+    {path:'header',component:Header},
+    {path:'footer',component:Footer},
+    {path:'about',component:About,title:'Cookpedia - About'},
+    {path:'contact',component:Contact,title:'Cookpedia - Contact'},
+    {path:'login',component:Login,title:'Cookpedia - Login'},
+    {path:'register',component:Register,title:'Cookpedia - Register'},
+    {path:'profile',component:Profile,title:'Cookpedia - Profile'},
+    {path:'recipies',component:Recipes,title:'Cookpedia - Recipies'},
+    {path:'saved-recipe',component:SavedRecipes,title:'Cookpedia - User Recipe'},
+    {path:'recipe/:id/view',component:ViewRecipe,title:'Cookpedia - Recipe'},
+    {path:'**',component:Pnf,title:'Page Not Found'},
+];
